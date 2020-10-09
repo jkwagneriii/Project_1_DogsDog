96223719f960535dc3a59040930fdd0ad1c9c4f7
Ryan Wills <68193699+ryanwills720@users.noreply.github.com>
2020-10-09 12:11:29 -0500

    removed extraneous javascriptCode2.js and pure.css example folder, formatted splash-subhead text on index.html

--- js/javascriptCode2.js
+++
@@ -1,9 +0,0 @@
-// have a global variable with an array of dog breeds that correspond with the chosen lifestyle
-
-// when a specific .html page is loaded, apply the array as a text list & also call a picture of each breed from the dog.ceo API
-
-// when an individual dog breed is clicked execute a search on the thedogapi.com API for the dog breed to pull up attributes
-
-// var animal = $(this).attr("data-animal");
-// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-// animal + "&api_key=dc6zaTOxFJmzC&limit=10";
